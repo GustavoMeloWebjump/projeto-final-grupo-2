3a7b692ac92364eb6944ab7f4516c8480a383887
GustavoMeloWebjump <gustavo.melo@webjump.com.br>
2021-08-26 13:53:06 -0300

    logic of discount cart done

--- app/code/Webjump/Backend/Setup/Patch/Data/InstallCartRule.php
+++ app/code/Webjump/Backend/Setup/Patch/Data/InstallCartRule.php
@@ -3,13 +3,13 @@ namespace Webjump\Backend\Setup\Patch\Data;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Rule\Model\ConditionFactory;
 use Magento\SalesRule\Model\RuleFactory;
 use Magento\SalesRule\Model\Rule\Condition\CombineFactory;
 use Magento\SalesRule\Model\ResourceModel\Rule as ResourceModelRule;
 use Magento\Framework\App\State;
 use Magento\Framework\App\Area;
-
+use Magento\SalesRule\Model\Rule\Condition\Address;
+use Magento\SalesRule\Model\Rule\Condition\AddressFactory;
 class InstallCartRule implements DataPatchInterface
 {
 
@@ -18,14 +18,16 @@ class InstallCartRule implements DataPatchInterface
     private $combineFactory;
     private $resourceModelRule;
     private $state;
+    private $addressFactory;
 
-    public function __construct(RuleFactory $ruleFactory, ModuleDataSetupInterface $moduleDataSetup, CombineFactory $combineFactory, ResourceModelRule $resourceModelRule, State $state)
+    public function __construct(RuleFactory $ruleFactory, ModuleDataSetupInterface $moduleDataSetup, CombineFactory $combineFactory, ResourceModelRule $resourceModelRule, State $state, AddressFactory $addressFactory)
     {
         $this->ruleFactory = $ruleFactory;
         $this->moduleDataSetup = $moduleDataSetup;
         $this->combineFactory = $combineFactory;
         $this->resourceModelRule = $resourceModelRule;
         $this->state = $state;
+        $this->addressFactory = $addressFactory;
     }
 
     public function apply()
@@ -33,24 +35,33 @@ class InstallCartRule implements DataPatchInterface
         $this->state->setAreaCode(Area::AREA_GLOBAL);
 
         $this->moduleDataSetup->getConnection()->startSetup();
+        $address = $this->addressFactory->create();
 
         $combine = $this->combineFactory->create();
         $combineCondtions = $this->combineFactory->create();
 
-        $combine
-            ->setData('value', '1')
-            ->addCondition($combineCondtions
-                ->setData('attribute', 'total_qty')
-                ->setData('operator', '>=')
-                ->setData('value', '5')
-            );
+
+        $address->settype(Address::class);
+        $address->setData('attribute', 'total_qty');
+        $address->setData('operator', '>=');
+        $address->setData('value', '5');
+
+        $combine->setData('attribute', 'null');
+        $combine->setData('operator', 'null');
+        $combine->setData('value', '1');
+        $combine->setData('aggregator', 'all');
+        $combine->setConditions([$address]);
 
         $cartRule = $this->ruleFactory->create();
 
+        $cartRule->setId(1);
         $cartRule->setName('discount for 5 itens or more in cart');
         $cartRule->setDescription('This discount will be aplied when the cart has 5 itens or more');
         $cartRule->setIsActive(1);
+        $cartRule->setWebsiteIds([1, 2]);
+        $cartRule->setCustomerGroupIds([0, 1, 2, 3]);
         $cartRule->setConditions($combine);
+        $cartRule->setDiscountAmount(10);
 
         $this->resourceModelRule->save($cartRule);
 

5e2ff0e2d438c268e996a53f7436d271a27d1b86
Beatriz <beatriz.varela@webjump.com.br>
2021-09-02 16:27:10 -0300

    pagina sobre traduzida

--- app/code/Webjump/SetupContents/Setup/Patch/Data/CreateAboutPagePatinhasEn.php
+++ app/code/Webjump/SetupContents/Setup/Patch/Data/CreateAboutPagePatinhasEn.php
@@ -10,7 +10,7 @@ use Magento\Store\Model\WebsiteFactory;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Store\Model\StoreManagerInterface;
 
-class CreateAboutPagePatinhas implements DataPatchInterface
+class CreateAboutPagePatinhasEn implements DataPatchInterface
 {
     /**
      * @var ModuleDataSetupInterface
@@ -87,18 +87,18 @@ class CreateAboutPagePatinhas implements DataPatchInterface
         $this->moduleDataSetup->getConnection()->startSetup();
 
         $content = <<<HTML
-        <style>#html-body [data-pb-style=UMX3GX3]{justify-content:flex-start;display:flex;flex-direction:column;background-position:left top;background-size:cover;background-repeat:no-repeat;background-attachment:scroll}</style><div data-content-type="row" data-appearance="contained" data-element="main"><div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="inner" data-pb-style="UMX3GX3"><h1 data-content-type="heading" data-appearance="default" data-element="main">Sobre Nós</h1><div data-content-type="text" data-appearance="default" data-element="main"><p>Nós somos um petshop de São Paulo, Brasil.</p>
+        <style>#html-body [data-pb-style=SE3DE2I]{justify-content:flex-start;display:flex;flex-direction:column;background-position:left top;background-size:cover;background-repeat:no-repeat;background-attachment:scroll}</style><div data-content-type="row" data-appearance="contained" data-element="main"><div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="inner" data-pb-style="SE3DE2I"><h1 data-content-type="heading" data-appearance="default" data-element="main">About Us</h1><div data-content-type="text" data-appearance="default" data-element="main"><p>We are a petshop based in São Paulo, Brasil.</p>
         <p>Lorem ipsum dolor sit amet. Est delectus voluptatem id voluptatem fugiat rem inventore facere ad porro dolores hic nostrum itaque. In cumque nobis et officiis laborum hic vitae quae non aliquam rerum eum numquam dolorem sit libero consectetur. Ut deleniti laudantium sit sunt rerum et atque internos! Repudiandae tempora est velit quisquam vel enim ipsum a corporis error et similique enim non aliquam consequuntur et enim quae!</p>
         <p>Ut sunt veritatis quo velit quae qui sapiente suscipit et asperiores quia. Aut consequuntur placeat nam quaerat possimus sed maxime autem est suscipit totam aut repellendus veritatis!</p>
         <p>At quae laborum sit quia dolor et rerum quia rem numquam voluptatem eum culpa labore ea beatae quasi? Ea unde doloremque eum dolorem officiis est ducimus quia 33 aspernatur magnam ab dolore mollitia At Quis voluptas cum dolor repellat. Aut dignissimos laudantium est praesentium quia et reiciendis voluptatem aut dolore blanditiis rem fugiat voluptatum aut odit ipsum. Et quidem iste id voluptatibus sunt in maxime rerum.</p></div></div></div>
-        HTML;
+       HTML;
 
-        $pageIdentifier = 'about';
+        $pageIdentifier = 'about-us';
         $cmsPageModel = $this->pageFactory->create()->load($pageIdentifier, 'title');
-        $cmsPageModel->setIdentifier('about');
+        $cmsPageModel->setIdentifier('about-us');
         $cmsPageModel->setStores($website->getStoreIds());
-        $cmsPageModel->setTitle('Quem somos');
-        $cmsPageModel->setContentHeading('Quem somos');
+        $cmsPageModel->setTitle('About us');
+        $cmsPageModel->setContentHeading('About us');
         $cmsPageModel->setPageLayout('1column');
         $cmsPageModel->setIsActive(1);
         $cmsPageModel->setContent($content)->save();

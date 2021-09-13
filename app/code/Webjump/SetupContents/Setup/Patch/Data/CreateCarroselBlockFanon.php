d2d043a6743c5e5bf391dcd5ae0c92c3bd461346
Matheus C. Reges <matheus.reges@webjump.com.br>
2021-09-13 15:20:59 -0300

    Corrigindo inconformidades no front end

--- app/code/Webjump/SetupContents/Setup/Patch/Data/CreateCarroselBlockFanon.php
+++ app/code/Webjump/SetupContents/Setup/Patch/Data/CreateCarroselBlockFanon.php
@@ -6,6 +6,7 @@ namespace Webjump\SetupContents\Setup\Patch\Data;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Store\Api\StoreRepositoryInterface;
+use Webjump\Backend\App\GetCategoriesByName;
 use Webjump\Backend\Setup\Patch\Data\InstallWGS;
 
 /**
@@ -28,6 +29,11 @@ class CreateCarroselBlockFanon implements DataPatchInterface
      * @var ModuleDataSetupInterface $moduleDataSetup
      */
     private $moduleDataSetup;
+
+      /**
+     * @var GetCategoriesByName $getCategoriesByName
+     */
+    private $getCategoriesByName;
 
     /**
      * @var BlockRepositoryInterface $blockRepository
@@ -54,12 +60,14 @@ class CreateCarroselBlockFanon implements DataPatchInterface
         ModuleDataSetupInterface $moduleDataSetup,
         \Magento\Cms\Api\BlockRepositoryInterface $blockRepository,
         \Magento\Cms\Api\Data\BlockInterfaceFactory $blockFactory,
-        StoreRepositoryInterface $storeRepository
+        StoreRepositoryInterface $storeRepository,
+        GetCategoriesByName $getCategoriesByName
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->blockRepository = $blockRepository;
         $this->blockFactory = $blockFactory;
         $this->storeRepository = $storeRepository;
+        $this->getCategoriesByName = $getCategoriesByName;
     }
 
     /**
@@ -70,8 +78,11 @@ class CreateCarroselBlockFanon implements DataPatchInterface
     public function apply(): void
     {
         $this->moduleDataSetup->startSetup();
+
+        $categoryId = $this->getCategoriesByName->getCategoryId('Lançamentos');
+    
         $content = <<<HTML
-        <style>#html-body [data-pb-style=CJW4D3N]{justify-content:flex-start;display:flex;flex-direction:column;background-position:left top;background-size:cover;background-repeat:no-repeat;background-attachment:scroll;margin:0}#html-body [data-pb-style=I5PLSKM]{text-align:center}</style><div class="row-style" data-content-type="row" data-appearance="full-width" data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="main" data-pb-style="CJW4D3N"><div class="row-full-width-inner" data-element="inner"><h3 data-content-type="heading" data-appearance="default" data-element="main" data-pb-style="I5PLSKM">Conheça nossos lançamentos!</h3><div class="banner-fanon-style" data-content-type="products" data-appearance="carousel" data-autoplay="true" data-autoplay-speed="1500" data-infinite-loop="true" data-show-arrows="true" data-show-dots="true" data-carousel-mode="default" data-center-padding="90px" data-element="main">{{widget type="Magento\CatalogWidget\Block\Product\ProductsList" template="Magento_PageBuilder::catalog/product/widget/content/carousel.phtml" anchor_text="" id_path="" show_pager="0" products_count="14" condition_option="category_ids" condition_option_value="10" type_name="Catalog Products Carousel" conditions_encoded="^[`1`:^[`aggregator`:`all`,`new_child`:``,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Combine`,`value`:`1`^],`1--1`:^[`operator`:`==`,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Product`,`attribute`:`category_ids`,`value`:`10`^]^]" sort_order="position"}}</div></div></div>
+        <style>#html-body [data-pb-style=CJW4D3N]{justify-content:flex-start;display:flex;flex-direction:column;background-position:left top;background-size:cover;background-repeat:no-repeat;background-attachment:scroll;margin:0}#html-body [data-pb-style=I5PLSKM]{text-align:center}</style><div class="row-style" data-content-type="row" data-appearance="full-width" data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="main" data-pb-style="CJW4D3N"><div class="row-full-width-inner" data-element="inner"><h3 data-content-type="heading" data-appearance="default" data-element="main" data-pb-style="I5PLSKM">Conheça nossos lançamentos!</h3><div class="banner-fanon-style" data-content-type="products" data-appearance="carousel" data-autoplay="true" data-autoplay-speed="1500" data-infinite-loop="true" data-show-arrows="true" data-show-dots="true" data-carousel-mode="default" data-center-padding="90px" data-element="main">{{widget type="Magento\CatalogWidget\Block\Product\ProductsList" template="Magento_PageBuilder::catalog/product/widget/content/carousel.phtml" anchor_text="" id_path="" show_pager="0" products_count="14" condition_option="category_ids" condition_option_value="$categoryId" type_name="Catalog Products Carousel" conditions_encoded="^[`1`:^[`aggregator`:`all`,`new_child`:``,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Combine`,`value`:`1`^],`1--1`:^[`operator`:`==`,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Product`,`attribute`:`category_ids`,`value`:`$categoryId`^]^]" sort_order="position"}}</div></div></div>
         HTML;
         $this->blockRepository->save($this->getCmsBlock($content));
         $this->moduleDataSetup->endSetup();
